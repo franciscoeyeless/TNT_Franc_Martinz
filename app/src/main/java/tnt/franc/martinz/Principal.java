fd4ec5a2bb609ad8363932fd97c154477811253d
franciscoeyeless <francisco.martinez1@virginiogomez.cl>
2023-10-25 17:43:32 -0300

    update 17:43

--- app/src/main/java/tnt/franc/martinz/Principal.java
+++ app/src/main/java/tnt/franc/martinz/Principal.java
@@ -1,14 +1,56 @@
 package tnt.franc.martinz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
-public class Principal extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.internal.ICameraUpdateFactoryDelegate;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Principal extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener {
+
+    EditText txtLatitud, txtLongitud;
+    GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        txtLatitud=findViewById(R.id.txtLatitud);
+        txtLatitud=findViewById(R.id.txtLongitud);
+
+        SupportMapFragment mapFragment= (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap= googleMap;
+        this.mMap.setOnMapClickListener(this);
+        this.mMap.setOnMapLongClickListener(this);
+
+        LatLng araucoStation = new LatLng(-36.608493, -72.103258);
+        mMap.addMarker(new MarkerOptions().position(araucoStation).title("Bicicletero Arauco"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(araucoStation));
+    }
+
+    @Override
+    public void onMapClick(@NonNull LatLng latLng) {
+        txtLatitud.setText(""+latLng.latitude);
+        txtLongitud.setText(""+latLng.longitude);
+    }
+
+    @Override
+    public void onMapLongClick(@NonNull LatLng latLng) {
+        txtLatitud.setText(""+latLng.latitude);
+        txtLongitud.setText(""+latLng.longitude);
     }
 }
